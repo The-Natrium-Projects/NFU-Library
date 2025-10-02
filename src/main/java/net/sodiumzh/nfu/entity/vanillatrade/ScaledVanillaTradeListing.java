14cf195ee5faa6e3931768bc1efcaeca60f60535
SodiumZH <46860829+SodiumZH@users.noreply.github.com>
2025-10-02 17:13:04 +0800

    Trades
    Now Vanilla Trade Registry can scale the weight of each collection
    Fixed amount error in generating offers from listing collection

---
+++ src/main/java/net/sodiumzh/nfu/entity/vanillatrade/ScaledVanillaTradeListing.java
@@ -0,0 +1,54 @@
+package net.sodiumzh.nfu.entity.vanillatrade;
+
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.item.trading.MerchantOffer;
+import org.jetbrains.annotations.Nullable;
+
+import javax.annotation.Nonnull;
+
+/**
+ * A wrapper of {@link IVanillaTradeListing} that can modify its selection weight without knowing the
+ * implementation or affecting the original instance.
+ * <p>Used in collection weight scaling in {@link VanillaTradeRegistry}.
+ */
+public class ScaledVanillaTradeListing implements IVanillaTradeListing {
+
+    private final IVanillaTradeListing original;
+    private double scale = 1;
+
+    public ScaledVanillaTradeListing(@Nonnull IVanillaTradeListing original, double scale) {
+        this.original = original;
+        this.scale = scale;
+    }
+
+    public ScaledVanillaTradeListing(@Nonnull IVanillaTradeListing original) {
+        this.original = original;
+    }
+
+    @Override
+    public double getSelectionWeight() {
+        return this.original.getSelectionWeight() * this.scale;
+    }
+
+    @Override
+    public boolean isValid() {
+        return this.original.isValid();
+    }
+
+    @Override
+    public int getDefaultRequiredLevel() {
+        return this.original.getDefaultRequiredLevel();
+    }
+
+    @Nonnull
+    @Override
+    public MerchantOffer getOffer(Entity pTrader, RandomSource pRandom) {
+        return original.getOffer(pTrader, pRandom);
+    }
+
+    @Override
+    public String toString() {
+        return "ScaledVanillaTradeListing {original = " + original + ", scale = " + this.scale + "}";
+    }
+}
--- src/main/java/net/sodiumzh/nfu/entity/vanillatrade/VanillaTradeRegistry.java
+++ src/main/java/net/sodiumzh/nfu/entity/vanillatrade/VanillaTradeRegistry.java
@@ -4,6 +4,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
 import com.google.common.collect.SetMultimap;
 import com.google.gson.JsonElement;
 import net.minecraft.resources.ResourceLocation;
@@ -14,7 +15,9 @@ import net.minecraftforge.fml.LogicalSide;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.sodiumzh.nfu.container.Tuple2;
 import net.sodiumzh.nfu.container.Tuple3;
+import net.sodiumzh.nfu.container.Tuple4;
 import net.sodiumzh.nfu.math.ThreadSafeRandomSource;
+import net.sodiumzh.nfu.object.Validatable;
 import net.sodiumzh.nfu.registry.NFURegistries;
 import net.sodiumzh.nfu.registry.NFURegistry;
 import net.sodiumzh.nfu.util.NFUDataStatics;
@@ -34,13 +37,14 @@ import javax.annotation.Nullable;
 public class VanillaTradeRegistry
 {
 	private static final RandomSource RND = new ThreadSafeRandomSource();
-	private final SetMultimap<Tuple2<ResourceLocation, VillagerProfession>, VanillaTradeListingCollection<?>> table;
+	// Table: {key, profession} -> {collection, weightScale}
+	private final Multimap<Tuple2<ResourceLocation, VillagerProfession>, Tuple2<VanillaTradeListingCollection<?>, Double>> table;
 
 	@Nullable
 	private ResourceLocation lastKey = null;
 	@Nonnull
 	private VillagerProfession lastProfession = VillagerProfession.NONE;
-	private VanillaTradeRegistry.Collected collectedCache = null;
+	private Validatable<VanillaTradeRegistry.Collected> collectedCache = new Validatable<>();
 
 	public VanillaTradeRegistry() {
 		this.table = HashMultimap.create();
@@ -50,13 +54,27 @@ public class VanillaTradeRegistry
 		this.lastProfession = VillagerProfession.NONE;
 	}
 
-	public VanillaTradeRegistry put(ResourceLocation key, VillagerProfession profession, VanillaTradeListingCollection<?>... values) {
-		for (VanillaTradeListingCollection<?> value: values) {
-			table.put(new Tuple2<>(key, profession), value);
-		}
+	public VanillaTradeRegistry put(ResourceLocation key, VillagerProfession profession, VanillaTradeListingCollection<?> collection, double weightScale) {
+		Tuple2<ResourceLocation, VillagerProfession> mapKey = new Tuple2<>(key, profession);
+		table.get(mapKey).stream().filter(e -> Objects.equals(e.getA(), collection))
+			.findFirst()
+			.ifPresentOrElse(
+				e -> e.setB(e.getB() + weightScale),
+				() -> table.put(mapKey, Tuple2.of(collection, weightScale)));
 		lastKey = key;
 		lastProfession = profession;
-		this.collectedCache = null;
+		this.collectedCache.invalidate();
+		return this;
+	}
+
+	public VanillaTradeRegistry put(ResourceLocation key, VanillaTradeListingCollection<?> value, double weightScale) {
+		return put(key, VillagerProfession.NONE, value, weightScale);
+	}
+
+	public VanillaTradeRegistry put(ResourceLocation key, VillagerProfession profession, VanillaTradeListingCollection<?>... collections) {
+		for (VanillaTradeListingCollection<?> value: collections) {
+			this.put(key, profession, value, 1d);
+		}
 		return this;
 	}
 
@@ -65,15 +83,20 @@ public class VanillaTradeRegistry
 	}
 
 	public VanillaTradeRegistry put(ResourceLocation key, VillagerProfession profession, Iterable<VanillaTradeListingCollection<?>> values) {
-		values.forEach(v -> table.put(new Tuple2<>(key, profession), v));
-		lastKey = key;
-		lastProfession = profession;
-		this.collectedCache = null;
+		values.forEach(v -> this.put(key, profession, v, 1d));
 		return this;
 	}
 
 	public VanillaTradeRegistry put(ResourceLocation key, Iterable<VanillaTradeListingCollection<?>> values) {
 		return put(key, VillagerProfession.NONE, values);
+	}
+
+	public VanillaTradeRegistry putLast(VillagerProfession profession, VanillaTradeListingCollection<?> value, double weightScale) {
+		if (lastKey == null) {
+			throw new IllegalStateException("VanillaTradeRegistry#putLast: missing last key. Specify a key by calling any " +
+				"key-specific version of put() before calling any key-omitted versions.");
+		}
+		return put(lastKey, profession, value, weightScale);
 	}
 
 	public VanillaTradeRegistry putLast(VillagerProfession profession, VanillaTradeListingCollection<?>... values) {
@@ -84,6 +107,14 @@ public class VanillaTradeRegistry
 		return put(lastKey, profession, values);
 	}
 
+	public VanillaTradeRegistry putLast(VanillaTradeListingCollection<?> value, double weightScale) {
+		if (lastKey == null) {
+			throw new IllegalStateException("VanillaTradeRegistry#putLast: missing last key. Specify a key by calling any " +
+				"key-specific version of put() before calling any key-omitted versions.");
+		}
+		return put(lastKey, lastProfession, value, weightScale);
+	}
+
 	public VanillaTradeRegistry putLast(VanillaTradeListingCollection<?>... values) {
 		if (lastKey == null) {
 			throw new IllegalStateException("VanillaTradeRegistry#putLast: missing last key. Specify a key by calling any " +
@@ -92,12 +123,16 @@ public class VanillaTradeRegistry
 		return put(lastKey, lastProfession, values);
 	}
 
-	public Set<VanillaTradeListingCollection<?>> getCollections(ResourceLocation key, VillagerProfession profession) {
-		return table.get(new Tuple2<>(key, profession));
+	public Map<VanillaTradeListingCollection<?>, Double> getCollections(ResourceLocation key, VillagerProfession profession) {
+		return table.get(new Tuple2<>(key, profession)).stream().collect(Collectors.toMap(Tuple2::getA, Tuple2::getB));
 	}
 
-	public Set<VanillaTradeListingCollection<?>> getCollectionsForDefaultProfession(ResourceLocation key) {
+	public Map<VanillaTradeListingCollection<?>, Double> getCollectionsForDefaultProfession(ResourceLocation key) {
 		return getCollections(key, VillagerProfession.NONE);
+	}
+
+	public Set<Tuple2<ResourceLocation, VillagerProfession>> keySet() {
+		return new HashSet<>(table.keySet());
 	}
 
 	/**
@@ -105,17 +140,33 @@ public class VanillaTradeRegistry
 	 * united listings from all listing collections.
 	 */
 	public Collected collect() {
-		if (collectedCache != null) return collectedCache;
+		if (collectedCache.isValidated()) return collectedCache.get();
 		Collected res = new Collected();
-		table.keySet().stream()
-				.map(k -> Tuple3.of(k, table.get(k)))
-				.forEach(entry -> {
-					entry.c.stream()
-						.map(VanillaTradeListingCollection::allLevelsAndListings)
-						.forEach(multimap -> multimap.keySet().forEach(level -> res.table.
-								putAll(Tuple3.of(entry.a, entry.b, level), multimap.get(level))));
-		});
-		collectedCache = res;
+		//table.keySet()	// {key, profession}
+			/*
+			.stream().map(k -> Tuple3.of(k, table.get(k)))	// {key, profession, (all collections and weight scales)}
+			// For each key-profession pair
+			.forEach(entry -> entry.c.stream() // {collection, weight scale}
+				// Map (collection + weight scale pair) to (level-listings multimap + weight scale pair)
+				.map(collectionWeightPair -> Tuple2.of(collectionWeightPair.getA().allLevelsAndListings(), collectionWeightPair.getB()))
+				// For each level-listing multimap, scale and collect
+				.forEach(multimapWeightPair -> multimapWeightPair.getA().keySet().forEach(
+					// For each level,
+					level -> res.table.putAll(Tuple3.of(entry.a, entry.b, level),
+						// Scale each listing before collecting
+						multimapWeightPair.getA().get(level).stream().map(listing -> new ScaledVanillaTradeListing(listing, multimapWeightPair.getB())).collect(Collectors.toList()))))
+		);*/
+		table.entries().stream().map(e -> Tuple3.of(e.getKey(), e.getValue().getA(), e.getValue().getB()))
+			// Expand to {(key-profession), level, listing, weightScale}
+			.flatMap(e -> e.b.allLevelsAndListings().entries().stream().map(levelAndListing -> Tuple4.of(e.a, levelAndListing.getKey(), levelAndListing.getValue(), e.c)))
+			// Remove entries in which weight is 0 or negative
+			.filter(e -> e.getD() > 0d)
+			// Scale the weight to each listing
+			.map(e -> Tuple4.of(e.a.getA(), e.a.getB(), e.b, new ScaledVanillaTradeListing(e.c, e.d)))
+			// Put each scaled listing
+			.forEach(e -> res.table.put(new Tuple3<>(e.a, e.b, e.c), e.d));
+
+		collectedCache.setAndValidate(res);
 		return res;
 	}
 
@@ -163,36 +214,95 @@ public class VanillaTradeRegistry
 		public IVanillaTradeListingCollection<IVanillaTradeListing> getForDefaultProfession(ResourceLocation key) {
 			return this.get(key, VillagerProfession.NONE);
 		}
+
+		public Set<Tuple2<ResourceLocation, VillagerProfession>> keySet() {
+			return table.keySet().stream().map(tp -> Tuple2.of(tp.a, tp.b)).collect(Collectors.toSet());
+		}
+
 	}
 
+	/**
+	 * Read data from a given location.
+	 * @param data Location of data json file.
+	 */
 	public VanillaTradeRegistry readData(ResourceLocation data) {
+		ResourceLocation oldLastKey = this.lastKey;
+		VillagerProfession oldLastProf = this.lastProfession;
+		// Data reading doesn't allow last key/prof actions
+		this.lastKey = null;
+		this.lastProfession = VillagerProfession.NONE;
+		// Allow omitting ".json"
 		ResourceLocation actualDataKey = data.toString().endsWith(".json")?
 				data : new ResourceLocation(data.getNamespace(), data.getPath() + ".json");
+
 		NFUDataStatics.readJsons(LogicalSide.SERVER, actualDataKey, json -> {
 			try {
 				json.getAsJsonArray().forEach(elem -> {
-					ResourceLocation lastKey = null;
-					VillagerProfession lastProf = VillagerProfession.NONE;
-					ResourceLocation currentKey = null;
+					ResourceLocation currentKey = null;	// For exception error information output
 					try {
-						ResourceLocation key =
-								NFUDataStatics.getOptional(elem.getAsJsonObject(), "key", JsonElement::getAsString)
-								.map(ResourceLocation::new).orElse(lastKey);
-						currentKey = key;
+						/*
+						Each array element stands for a registry entry, it can be either an object or a primitive.
+						Object has format:
+						{"key": "some_mod:some_key", "profession": "some_mod:some_profession", "collection": [...]}
+						in which profession and collection can be omitted, and default profession is NONE.
+						Primitive (string) stands for the key and anything else is default.
+						Note that the collection with the identical key of the entry will be auto added.
+
+						The "collection" argument is an array, of which the element is either
+						primitive (string) or like: {"key": "some_mod:collection_key", "weight": 2.0}.
+						If primitive, the value is string key and weight is 1.0.
+						When "collection" is single-element, it can be simplified as a string primitive.
+						*/
+						// Read key
+						ResourceLocation key = null;
+						if (elem.isJsonObject())
+							key = NFUDataStatics.getOptional(elem.getAsJsonObject(), "key", JsonElement::getAsString)
+								.map(ResourceLocation::new).orElse(null);
+						else if (elem.isJsonPrimitive())
+							key = new ResourceLocation(elem.getAsString());
 						if (key == null) return;
-						VillagerProfession prof =
-								NFUDataStatics.getOptional(elem.getAsJsonObject(), "profession", JsonElement::getAsString)
-										.map(ResourceLocation::new)
-										.map(ForgeRegistries.VILLAGER_PROFESSIONS::getValue)
-										.orElse(lastProf);
-						Set<VanillaTradeListingCollection<?>> collections =
-								NFUDataStatics.getOptionalList(elem.getAsJsonObject(), "collections", JsonElement::getAsString,
-                                        e -> e.isJsonPrimitive() && e.getAsJsonPrimitive().isString()).stream()
-                                .map(k -> NFURegistries.VANILLA_TRADE_LISTING_COLLECTIONS.getValue(new ResourceLocation(k)))
-                                .filter(Objects::nonNull).collect(Collectors.toSet());
-						this.put(key, prof, collections);
-						this.lastKey = key;
-						this.lastProfession = prof;
+						currentKey = key;
+						// Read profession
+						VillagerProfession prof = VillagerProfession.NONE;
+						if (elem.isJsonObject()) {
+							prof = NFUDataStatics.getOptional(elem.getAsJsonObject(), "profession", JsonElement::getAsString)
+								.map(ResourceLocation::new)
+								.map(ForgeRegistries.VILLAGER_PROFESSIONS::getValue)
+								.orElse(VillagerProfession.NONE);
+						}
+
+						// == Read collections == //
+						Map<ResourceLocation, Double> collectionKeysAndWeights = new HashMap<>();
+						if (elem.isJsonObject()) {
+							// Resolve each collection and weight. Method getOptionalList handles primitive/object
+							// as single-element array, so no need to distinguish them here
+							NFUDataStatics.getOptionalList(elem.getAsJsonObject(), "collections", e -> {
+								if (e.isJsonObject()) {
+									String collectionKey = e.getAsJsonObject().get("key").getAsString();
+									double weight = NFUDataStatics.getOptionalDouble(e.getAsJsonObject(), "weight")
+										.orElse(1d);
+									return Tuple2.of(collectionKey, weight);
+								}
+								else if (e.isJsonPrimitive())
+									return Tuple2.of(e.getAsString(), 1d);
+								else return null;
+							}, e -> (e.isJsonObject() && e.getAsJsonObject().has("key")) || e.isJsonPrimitive())
+								.stream().filter(Objects::nonNull)
+								.forEach(entry -> collectionKeysAndWeights.put(new ResourceLocation(entry.getA()), entry.getB()));
+						}
+						else if (elem.isJsonPrimitive())
+							collectionKeysAndWeights.put(new ResourceLocation(elem.getAsString()), 1d);
+						if (!collectionKeysAndWeights.containsKey(key))	// Include the file with the same name by default
+							collectionKeysAndWeights.put(key, 1d);
+						var collectionsAndWeights = collectionKeysAndWeights.entrySet().stream()
+							.map(entry -> Tuple2.of(NFURegistries.VANILLA_TRADE_LISTING_COLLECTIONS.getValue(entry.getKey()), entry.getValue()))
+							.filter(entry -> entry.getA() != null).toList();
+						for (var entry: collectionsAndWeights) {
+							this.put(key, prof, entry.getA(), entry.getB());
+						}
+						// Set back last key/prof records. Data reading doesn't allow last key/prof actions
+						this.lastKey = oldLastKey;
+						this.lastProfession = oldLastProf;
 					} catch (Exception e) {
 						NFUDebugStatics.errorOnce(VanillaTradeRegistry.class,
 								String.format("Read data failed, entry '%s' skipped. Exception: \n%s",
@@ -205,6 +315,8 @@ public class VanillaTradeRegistry
 						String.format("Read data failed, json '%s' skipped. Exception: \n%s", data.toString(), e.getMessage()));
 			}
 		});
+		this.lastKey = oldLastKey;
+		this.lastProfession = oldLastProf;
 		return this;
 	}
 
@@ -215,7 +327,10 @@ public class VanillaTradeRegistry
 	}
 
 	public List<ResourceLocation> getAllCollectionKeys(ResourceLocation key, VillagerProfession profession) {
-		return this.getCollections(key, profession).stream().map(VanillaTradeListingCollection::getRegistryKey).map(o -> o.orElse(new ResourceLocation("not:registered"))).collect(Collectors.toList());
+		return this.getCollections(key, profession).keySet()
+			.stream().map(VanillaTradeListingCollection::getRegistryKey)
+			.map(o -> o.orElse(new ResourceLocation("not:registered")))
+			.collect(Collectors.toList());
 	}
 
 }
