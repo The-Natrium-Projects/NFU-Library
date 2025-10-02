81060566894fa5a4db96a2a0a51fffe832fe534a
SodiumZH <46860829+SodiumZH@users.noreply.github.com>
2025-10-02 17:10:44 +0800

    Mixin features
    Split EntityTickEvent to 4 events
    Added current ticking entity accessor

--- src/main/java/net/sodiumzh/nfu/mixin/mixin/NFUMixinEntity.java
+++ src/main/java/net/sodiumzh/nfu/mixin/mixin/NFUMixinEntity.java
@@ -1,21 +1,31 @@
 package net.sodiumzh.nfu.mixin.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.sodiumzh.nfu.mixin.event.entity.*;
+import net.sodiumzh.nfu.mixin.event.entity.*;
+import net.sodiumzh.nfu.util.NFUEntityStatics;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraftforge.common.MinecraftForge;
 import net.sodiumzh.nfu.mixin.NFUMixin;
 import net.sodiumzh.nfu.mixin.NFUMixinHooks;
-import net.sodiumzh.nfu.mixin.event.entity.*;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Entity.class)
-public class NFUMixinEntity implements NFUMixin<Entity> {
+public abstract class NFUMixinEntity implements NFUMixin<Entity> {
+
+	@Shadow public abstract InteractionResult interact(Player pPlayer, InteractionHand pHand);
 
 	@Inject(at = @At("HEAD"), method = "hurt(Lnet/minecraft/world/damagesource/DamageSource;F)Z", cancellable = true)
 	private void hurt(DamageSource src, float amount, CallbackInfoReturnable<Boolean> callback)
@@ -25,9 +35,16 @@ public class NFUMixinEntity implements NFUMixin<Entity> {
 	}
 
 	@Inject(at = @At("HEAD"), method = "tick()V")
-	private void tick(CallbackInfo callback)
+	private void startBaseTick(CallbackInfo callback)
 	{
 		MinecraftForge.EVENT_BUS.post(new EntityTickEvent(caller()));
+		MinecraftForge.EVENT_BUS.post(new EntityStartBaseTickEvent(caller()));
+	}
+
+	@Inject(at = @At("TAIL"), method = "tick()V")
+	private void finishBaseTick(CallbackInfo callback)
+	{
+		MinecraftForge.EVENT_BUS.post(new EntityFinishBaseTickEvent(caller()));
 	}
 
 	@Inject(at = @At("HEAD"), method = "load(Lnet/minecraft/nbt/CompoundTag;)V")
@@ -60,5 +77,15 @@ public class NFUMixinEntity implements NFUMixin<Entity> {
 	@Inject(method = "discard()V", at = @At("HEAD"))
 	private void onDiscard(CallbackInfo ci) {
 		MinecraftForge.EVENT_BUS.post(new EntityDiscardEvent(caller()));
+	}
+
+	@WrapOperation(method = "rideTick()V",
+		at = @At(value = "INVOKE", target = "net/minecraft/world/entity/Entity.tick()V"))
+	private void onRideTick(Entity instance, Operation<Void> original) {
+		NFUEntityStatics.notifyEntityTickStart(instance);
+		MinecraftForge.EVENT_BUS.post(new EntityStartTickEvent(instance));
+		original.call(instance);
+		MinecraftForge.EVENT_BUS.post(new EntityFinishTickEvent(instance));
+		NFUEntityStatics.notifyEntityTickEnd(instance);
 	}
 }
