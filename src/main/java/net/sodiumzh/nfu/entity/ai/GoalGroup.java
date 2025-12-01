45f0a62e38c9fb1626a33e0f8659e2e694f35409
SodiumZH <46860829+SodiumZH@users.noreply.github.com>
2025-12-02 02:34:20 +0800

    Misc

---
+++ src/main/java/net/sodiumzh/nfu/entity/ai/GoalGroup.java
@@ -0,0 +1,42 @@
+package net.sodiumzh.nfu.entity.ai;
+
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.ai.goal.Goal;
+import net.minecraft.world.entity.ai.goal.target.TargetGoal;
+import net.sodiumzh.nfu.container.Tuple2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public class GoalGroup<T extends Mob> {
+
+    private List<Tuple2<Integer, Function<T, Goal>>> goals = new ArrayList<>();
+
+    /**
+     * Add the goal group to a mob.
+     * @param priorityOffset Priorities will be added this value.
+     */
+    public void addTo(T mob, int priorityOffset) {
+        for (var goal: goals) {
+            Goal inst = goal.getB().apply(mob);
+            if (inst instanceof TargetGoal tg) {
+                mob.targetSelector.addGoal(goal.getA() + priorityOffset, inst);
+            }
+            else {
+                mob.goalSelector.addGoal(goal.getA() + priorityOffset, inst);
+            }
+        }
+    }
+
+    public GoalGroup<T> addGoal(int priority, Function<? super T, Goal> goal) {
+        goals.add(Tuple2.of(priority, goal::apply));
+        return this;
+    }
+
+    public List<Tuple2<Integer, Function<T, Goal>>> getGoalsAndSuppliers() {
+        return List.copyOf(goals);
+    }
+
+}
