8306bda74c50a62ef484234c2f391bbe8122a1e6
SodiumZH <46860829+SodiumZH@users.noreply.github.com>
2025-12-14 15:29:44 +0800

    Revert "fix enchanted book trades being broken"

--- src/main/java/net/sodiumzh/nfu/entity/vanillatrade/VanillaTradeListingEnchanted.java
+++ src/main/java/net/sodiumzh/nfu/entity/vanillatrade/VanillaTradeListingEnchanted.java
@@ -3,11 +3,9 @@ package net.sodiumzh.nfu.entity.vanillatrade;
 import net.minecraft.util.RandomSource;
 import net.minecraft.util.Tuple;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.item.EnchantedBookItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.enchantment.Enchantment;
-import net.minecraft.world.item.enchantment.EnchantmentInstance;
 import net.minecraft.world.item.trading.MerchantOffer;
 import net.sodiumzh.nfu.container.Tuple2;
 import net.sodiumzh.nfu.math.RangedRandomInt;
@@ -69,7 +67,7 @@ public class VanillaTradeListingEnchanted extends VanillaTradeListing
 	{
 		MerchantOffer offer = super.getOffer(trader, rnd);
 		Tuple<Enchantment, Integer> e = selector.select();
-        EnchantedBookItem.addEnchantment(offer.getResult(), new EnchantmentInstance(e.getA(), e.getB()));
+		offer.getResult().enchant(e.getA(), e.getB());
 		return offer;
 	}
 
