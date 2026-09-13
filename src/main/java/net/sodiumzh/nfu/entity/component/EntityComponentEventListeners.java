5a5a2e9b62a59eded38978f5e6161c31d7ee81cf
SodiumZH <sodiumzh@outlook.com>
2026-09-13 14:50:05 +0800

    Fix synching frequency

--- src/main/java/net/sodiumzh/nfu/entity/component/EntityComponentEventListeners.java
+++ src/main/java/net/sodiumzh/nfu/entity/component/EntityComponentEventListeners.java
@@ -57,8 +57,12 @@ public class EntityComponentEventListeners {
     }
 
     @SubscribeEvent
-    public static void onLevelTick(TickEvent.WorldTickEvent event) {
-        if (event.side.equals(LogicalSide.SERVER) && event.world instanceof ServerLevel sl) {
+    public static void onLevelTick(TickEvent.LevelTickEvent event) {
+        // This frequency check controls packet sending frequency. Per-component frequency is further checked
+        // in
+        if (event.level.getGameTime() % NFUConfigs.CACHED_ENTITY_SYNCHER_FREQUENCY != 0)
+            return;
+        if (!event.level.isClientSide() && event.level instanceof ServerLevel sl) {
             if (event.phase.equals(TickEvent.Phase.START))
                 EntitySyncherComponent.syncAll(sl, false);
         }
