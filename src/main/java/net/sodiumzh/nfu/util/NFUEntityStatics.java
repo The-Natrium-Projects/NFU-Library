e4397d325b6ba7783a0c01d9860d5bc7ddea0057
SodiumZH <sodiumzh@outlook.com>
2026-09-19 01:55:07 +0800

    Fix typo

--- src/main/java/net/sodiumzh/nfu/util/NFUEntityStatics.java
+++ src/main/java/net/sodiumzh/nfu/util/NFUEntityStatics.java
@@ -75,7 +75,7 @@ import java.util.function.Predicate;
 public class NFUEntityStatics
 {
 	// Use as stack
-	private static final SideLocal<Deque<Entity>> TICKING_ENTITY = new SideLocal.withInitial(ArrayDeque::new);
+	private static final SideLocal<Deque<Entity>> TICKING_ENTITY = new SideLocal<>(ArrayDeque::new);
 
 	/**
 	 * Get the entities being ticked. Empty if it's not currently running in an entity ticking cycle.
@@ -89,6 +89,10 @@ public class NFUEntityStatics
 	 */
 	public static Deque<Entity> getEntityTickStack() {
 		return Optional.ofNullable(TICKING_ENTITY.get()).orElseGet(ArrayDeque::new);
+	}
+
+	public static Optional<Entity> getCurrentTickingEntity() {
+		return Optional.ofNullable(TICKING_ENTITY.get().peekFirst());
 	}
 
 	/**
