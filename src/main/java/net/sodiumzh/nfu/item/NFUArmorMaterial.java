3f5c9c18c99ac98885e300b49211eac5e4dfb0d4
SodiumZH <sodiumzh@outlook.com>
2026-09-25 01:40:31 +0800

    Update NFUArmorMaterial.java

--- src/main/java/net/sodiumzh/nfu/item/NFUArmorMaterial.java
+++ src/main/java/net/sodiumzh/nfu/item/NFUArmorMaterial.java
@@ -5,6 +5,7 @@ import com.google.common.collect.BiMap;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.util.LazyLoadedValue;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.ArmorMaterials;
@@ -17,11 +18,11 @@ import java.util.stream.Collectors;
 
 public class NFUArmorMaterial implements ArmorMaterial {
 
-    protected static final ArmorItem.Type[] TYPE_BY_INDEX = new ArmorItem.Type[]{
-        ArmorItem.Type.BOOTS, ArmorItem.Type.LEGGINGS, ArmorItem.Type.CHESTPLATE, ArmorItem.Type.HELMET
+    protected static final EquipmentSlot[] TYPE_BY_INDEX = new EquipmentSlot[]{
+        EquipmentSlot.FEET, EquipmentSlot.LEGS, EquipmentSlot.CHEST, EquipmentSlot.HEAD
     };
-    protected static final Map<ArmorItem.Type, Integer> TYPE_TO_INDEX =
-        Map.of(ArmorItem.Type.BOOTS, 0, ArmorItem.Type.LEGGINGS, 1, ArmorItem.Type.CHESTPLATE, 2, ArmorItem.Type.HELMET, 3);
+    protected static final Map<EquipmentSlot, Integer> TYPE_TO_INDEX =
+        Map.of(EquipmentSlot.FEET, 0, EquipmentSlot.LEGS, 1, EquipmentSlot.CHEST, 2, EquipmentSlot.HEAD, 3);
     protected final int[] durabilities;
     protected ResourceLocation name;
     protected final int[] slotProtections;
@@ -53,20 +54,20 @@ public class NFUArmorMaterial implements ArmorMaterial {
     }
 
     public static int[] durabilitiesOf(ArmorMaterial material) {
-        return Arrays.stream(TYPE_BY_INDEX).mapToInt(material::getDurabilityForType).toArray();
+        return Arrays.stream(TYPE_BY_INDEX).mapToInt(material::getDurabilityForSlot).toArray();
     }
 
     public static int[] defensesOf(ArmorMaterial material) {
-        return Arrays.stream(TYPE_BY_INDEX).mapToInt(material::getDefenseForType).toArray();
+        return Arrays.stream(TYPE_BY_INDEX).mapToInt(material::getDefenseForSlot).toArray();
     }
 
     @Override
-    public int getDurabilityForType(ArmorItem.Type type) {
+    public int getDurabilityForSlot(EquipmentSlot type) {
         return durabilities[TYPE_TO_INDEX.get(type)];
     }
 
     @Override
-    public int getDefenseForType(ArmorItem.Type type) {
+    public int getDefenseForSlot(EquipmentSlot type) {
         return this.slotProtections[TYPE_TO_INDEX.get(type)];
     }
 
